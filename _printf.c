31622e26112b3327d04216ec134f202f311340a6
unknown <toyinadesina60@gmail.com>
2022-10-18 12:04:36 +0100

    _printf now returns an int value

--- _printf.c
+++ _printf.c
@@ -1,5 +1,5 @@
-#include <stdarq.h>
 #include "main.h"
+#include <stdarg.h>
 
 /**
  * _printf -  a function to mimic the printf fro
@@ -8,49 +8,46 @@
  * ...: other inputs
  * return: nothing
  **/
+int printed_char = 0;
+int _printf(const char *format, ...) {
+  va_list arg_p;
+  const char *pp;
+  char *s;
+  char c;
+  int int_val;
 
-int _printf(const char *format, ...)
-{
-	va_list arg_p;
-    char *pp;
-    char *s;
-    char c;
-    int int_val;
+  va_start(arg_p, format);
+  for (pp = format; *pp; pp++) {
+    if (*pp != '%') {
+      _putchar(*pp);
+      printed_char++;
+      continue;
+    }
 
-	va_start(arg_p, format);
-	for (pp = format; *pp; pp++)
-	{
-		if (*pp != '%')
-		{
-			_putchar(*pp);
-			continue;
-		}
-
-		switch (*++pp)
-		{
-			case 'd':
-				int_val = va_arg(arg_p, int);
-				print_digits(int_val);
-				break;
-			case 'i':
-				int_val = va_arg(arg_p, int);
-				print_digits(int_val);
-				break;
-			case 'c':
-				c = va_arg(arg_p, int);
-				_putchar(c);
-				break;
-			case 's':
-				s = va_arg(arg_p, char *);
-				_puts_recursion(s);
-				break;
-			default:
-				_putchar(*pp);
-				break;
-		}
-
-	}
-	va_end(arg_p);
+    switch (*++pp) {
+    case 'd':
+      int_val = va_arg(arg_p, int);
+      print_digits(int_val);
+      break;
+    case 'i':
+      int_val = va_arg(arg_p, int);
+      print_digits(int_val);
+      break;
+    case 'c':
+      c = va_arg(arg_p, int);
+      _putchar(c);
+      break;
+    case 's':
+      s = va_arg(arg_p, char *);
+      _puts_recursion(s);
+      break;
+    default:
+      _putchar(*pp);
+      break;
+    }
+  }
+  va_end(arg_p);
+  return (printed_char);
 }
 
 /**
@@ -59,21 +56,22 @@ int _printf(const char *format, ...)
  * Return: void
  **/
 
-void print_digits(int n)
-{
-	// If number is smaller than 0, put a - sign
-	// and change number to positive
-	if (n < 0) {
-		_putchar('-');
-		n = -n;
-	}
+void print_digits(int n) {
+  // If number is smaller than 0, put a - sign
+  // and change number to positive
+  if (n < 0) {
+    _putchar('-');
+    printed_char++;
+    n = -n;
+  }
 
-	// Remove the last digit and recur
-	if (n/10)
-		print_digits(n/10);
+  // Remove the last digit and recur
+  if (n / 10)
+    print_digits(n / 10);
 
-	// Print the last digit
-	_putchar(n%10 + '0');
+  // Print the last digit
+  _putchar(n % 10 + '0');
+  printed_char++;
 }
 
 /**
@@ -82,14 +80,12 @@ void print_digits(int n)
  * Return: void
  */
 
-void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
+void _puts_recursion(char *s) {
+  if (*s == '\0') {
+    return;
+  }
 
-	_putchar(*s);
-	_puts_recursion(++s);
+  _putchar(*s);
+  printed_char++;
+  _puts_recursion(++s);
 }
